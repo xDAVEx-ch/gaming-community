09cd6cab4c9891b093de1792771cc3caf649dfd9
Dave Ch <dabbidchavarria@gmail.com>
2021-06-15 00:21:17 -0600

    feat: :construction: Create pre-order section
    Create and add pre-orders section in landing page

---
+++ src/components/pre-orders/pre-orders.component.jsx
@@ -0,0 +1,107 @@
+//React imports
+import React from 'react';
+
+//Vendors imports
+import { Grid, Cell, Button, Alignments, Colors } from 'react-foundation';
+
+import { Link } from 'react-router-dom';
+
+//My own imports
+import cover1 from './../../assets/alice-madness-cover.jpg';
+import cover2 from './../../assets/bully.jpg';
+import cover3 from './../../assets/crash-remaster.jpg';
+import cover4 from './../../assets/driver-san-francisco.jpg';
+import cover5 from './../../assets/gta-cover.jpg';
+import cover6 from './../../assets/kf2.jpg';
+
+import './pre-orders.styles.scss';
+
+const PreOrders = () => (
+  <section className='bg-split'>
+
+    <Grid>
+
+      <Cell medium='4' className='align-self-middle'>
+
+        <h2>Pre-orders</h2>
+        <p className='pre-order-text-description'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Labore officiis incidunt quod a nulla impedit. Iusto consequuntur nam dicta debitis at ut,
+          maxime vero sequi temporibus eum, odit, nemo id.
+        </p>
+
+        <Link to='/catalog'>
+          <Button color={Colors.SECONDARY}>View More</Button>
+        </Link>
+
+      </Cell>
+
+      <Cell medium='8'>
+
+        {/* ------ First inner grid -------*/}
+
+        <Grid className='pre-order-grid grid-margin-x' alignX={Alignments.RIGHT}>
+
+          <Cell medium='3'>
+            <div className='pre-order-game-cover'><img src={cover1} alt='cover' /></div>
+            <p className='pre-order-game-title'>
+              Alice Madness Returns
+              <span>$40.99</span>
+            </p>
+          </Cell>
+
+          <Cell medium='3'>
+            <div className='pre-order-game-cover'><img src={cover2} alt='cover' /></div>
+            <p className='pre-order-game-title'>
+              Bully: Scholarship Edition
+              <span>$40.99</span>
+            </p>
+          </Cell>
+
+          <Cell medium='3'>
+            <div className='pre-order-game-cover'><img src={cover3} alt='cover' /></div>
+            <p className='pre-order-game-title'>
+              Crash N'Sanity Trilogy
+              <span>$40.99</span>
+            </p>
+          </Cell>
+
+        </Grid>
+
+        {/* ------ Second inner grid -------*/}
+
+        <Grid className='pre-order-grid grid-margin-x' alignX={Alignments.RIGHT}>
+
+          <Cell medium='3'>
+            <div className='pre-order-game-cover'><img src={cover4} alt='cover' /></div>
+            <p className='pre-order-game-title'>
+              Driver San Francisco
+              <span>$40.99</span>
+            </p>
+          </Cell>
+
+          <Cell medium='3'>
+            <div className='pre-order-game-cover'><img src={cover5} alt='cover' /></div>
+            <p className='pre-order-game-title'>
+              Grand Theft Auto V
+              <span>$40.99</span>
+            </p>
+          </Cell>
+
+          <Cell medium='3'>
+            <div className='pre-order-game-cover'><img src={cover6} alt='cover' /></div>
+            <p className='pre-order-game-title'>
+              Killing Floor 2
+              <span>$40.99</span>
+            </p>
+          </Cell>
+
+        </Grid>
+
+      </Cell>
+
+    </Grid>
+  </section>
+);
+
+export default PreOrders;
--- src/pages/landing-page/landing-page.component.jsx
+++ src/pages/landing-page/landing-page.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Header from './../../components/header';
 import Hero from './../../components/hero';
+import PreOrders from './../../components/pre-orders';
 
 const LandingPage = () =>(
   <>
     <Header />
     <Hero />
+    <PreOrders />
   </>
 );
 
