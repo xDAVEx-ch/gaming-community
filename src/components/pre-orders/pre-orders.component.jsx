61b6f87fde7989c6cd7c8d50be6d2df2de8b9001
Dave Ch <dabbidchavarria@gmail.com>
2021-06-22 02:01:40 -0600

    fix: Generalize class names

--- src/components/pre-orders/pre-orders.component.jsx
+++ src/components/pre-orders/pre-orders.component.jsx
@@ -24,7 +24,7 @@ const PreOrders = () => (
       <Cell medium='4' className='align-self-middle pre-order-description'>
 
         <h2>Pre-orders</h2>
-        <p className='pre-order-text-description'>
+        <p className='text-description'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Labore officiis incidunt quod a nulla impedit. Iusto consequuntur nam dicta debitis at ut,
           maxime vero sequi temporibus eum, odit, nemo id.
@@ -40,27 +40,27 @@ const PreOrders = () => (
 
         {/* ------ First inner grid -------*/}
 
-        <Grid className='pre-order-game-grid grid-margin-x' alignX={Alignments.RIGHT}>
+        <Grid className='game-grid grid-margin-x' alignX={Alignments.RIGHT}>
 
           <Cell small='4' medium='3'>
-            <div className='pre-order-game-cover'><img src={cover1} alt='cover' /></div>
-            <p className='pre-order-game-title'>
+            <div className='game-cover'><img src={cover1} alt='cover' /></div>
+            <p className='game-title'>
               Alice Madness Returns
               <span>$40.99</span>
             </p>
           </Cell>
 
           <Cell small='4' medium='3'>
-            <div className='pre-order-game-cover'><img src={cover2} alt='cover' /></div>
-            <p className='pre-order-game-title'>
+            <div className='game-cover'><img src={cover2} alt='cover' /></div>
+            <p className='game-title'>
               Bully: Scholarship Edition
               <span>$40.99</span>
             </p>
           </Cell>
 
           <Cell small='4' medium='3'>
-            <div className='pre-order-game-cover'><img src={cover3} alt='cover' /></div>
-            <p className='pre-order-game-title'>
+            <div className='game-cover'><img src={cover3} alt='cover' /></div>
+            <p className='game-title'>
               Crash N'Sanity Trilogy
               <span>$40.99</span>
             </p>
@@ -70,27 +70,27 @@ const PreOrders = () => (
 
         {/* ------ Second inner grid -------*/}
 
-        <Grid className='pre-order-grid grid-margin-x' alignX={Alignments.RIGHT}>
+        <Grid className='game-grid grid-margin-x' alignX={Alignments.RIGHT}>
 
           <Cell small='4' medium='3'>
-            <div className='pre-order-game-cover'><img src={cover4} alt='cover' /></div>
-            <p className='pre-order-game-title'>
+            <div className='game-cover'><img src={cover4} alt='cover' /></div>
+            <p className='game-title'>
               Driver San Francisco
               <span>$40.99</span>
             </p>
           </Cell>
 
           <Cell small='4' medium='3'>
-            <div className='pre-order-game-cover'><img src={cover5} alt='cover' /></div>
-            <p className='pre-order-game-title'>
+            <div className='game-cover'><img src={cover5} alt='cover' /></div>
+            <p className='game-title'>
               Grand Theft Auto V
               <span>$40.99</span>
             </p>
           </Cell>
 
           <Cell small='4' medium='3'>
-            <div className='pre-order-game-cover'><img src={cover6} alt='cover' /></div>
-            <p className='pre-order-game-title'>
+            <div className='game-cover'><img src={cover6} alt='cover' /></div>
+            <p className='game-title'>
               Killing Floor 2
               <span>$40.99</span>
             </p>
