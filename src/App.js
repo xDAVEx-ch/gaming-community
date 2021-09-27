7c6cf2abd3991ea1b6ab5fc68622f7d66dc300df
Dave Ch <dabbidchavarria@gmail.com>
2021-09-27 00:09:16 -0600

    feat: Add new sign up page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import LandingPage from './pages/landing-page';
+import SignUpPage from './pages/sign-up-page';
 
 function App() {
   return (
     <>
       <Route exact path='/' component={ LandingPage }/>
+      <Route exact path='/signup' component={ SignUpPage }/>
     </>
   );
 }
---
+++ src/pages/sign-up-page/Sign-up-page.component.jsx
@@ -0,0 +1,17 @@
+//React imports
+import React from 'react';
+
+//My imports
+import Header from './../../components/header';
+import SignUpForm from './../../components/sign-up-form';
+import Footer from './../../components/footer';
+
+const SignUpPage = () => (
+  <main>
+    <Header />
+    <SignUpForm />
+    <Footer />
+  </main>
+);
+
+export default SignUpPage;
